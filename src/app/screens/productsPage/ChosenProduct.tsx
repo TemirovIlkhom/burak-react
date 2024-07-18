a52c8bd5b72c12e214b5bbd21134b1364407b1c3
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-07-18 16:55:56 +0900

    feat: publish chosen product sectional component

--- src/app/screens/productsPage/ChosenProduct.tsx
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -1,59 +1,65 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import CardMedia from '@mui/material/CardMedia';
-import Card from '@mui/material/Card';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import Rating from '@mui/material/Rating';
+import React from "react";
+import { Container, Stack, Box } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import Divider from "../../components/divider";
+import Button from "@mui/material/Button";
+import Rating from "@mui/material/Rating";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper";
 
 export default function ChosenProduct() {
-    return ( 
+  return (
     <div className={"chosen-product"}>
-        <Container>
-            <div className="chosen-box">
-        <span>Product Detail</span>
-            
-            <Stack className="product-box">
-                <Box className="left-box">
-                <Card sx={{ maxWidth: 600 }}>
-                 <CardMedia
-                     component="img"
-                     alt="green iguana"
-                     height="480"
-                     image="/img/fresh.webp"
-                     />
-                 </Card>             
-                </Box>
-
-
-                <Box className="right-box">
-                <span className="title">FR Kebab</span>
-                <div className="rating">
-                <Rating 
-                className="half-rating"
-                defaultValue={2.5} 
-                precision={0.5} 
-                />
-                <div className="eye">
-                <VisibilityIcon/>
-                10 pc 
+      <Box className={"title"}>Product Detail</Box>
+      <Container className={"product-container"}>
+        <Stack className={"chosen-product-slider"}>
+          <Swiper
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="swiper-area"
+          >
+            {["/img/cutlet.webp", "/img/kebab-fresh.webp"].map(
+              (ele: string, index: number) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <img className="slider-image" src={ele} />
+                  </SwiperSlide>
+                );
+              }
+            )}
+          </Swiper>
+        </Stack>
+        <Stack className={"chosen-product-info"}>
+          <Box className={"info-box"}>
+            <strong className={"product-name"}>Kebab</strong>
+            <span className={"resto-name"}>Burak</span>
+            <Box className={"rating-box"}>
+              <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+              <div className={"evaluation-box"}>
+                <div className={"product-view"}>
+                  <RemoveRedEyeIcon sx={{ mr: "10px" }} />
+                  <span>20</span>
                 </div>
-                </div>
-                <div className="desc">This is Kebab with Fruits.</div>
-                <div className="line"></div>
-                <Box className="price-box">
-                    <span>Price:</span>
-                    <span>$ 15</span>
-                </Box>
-                <Button
-                            variant={"contained"}
-                            color={"primary"}
-                            sx={{ width: 221.878,
-                                height: 46.726 }}
-                            >
-                                ADD TO BASKET
-                            </Button>
-                </Box>
-            </Stack>
+              </div>
+            </Box>
+            <p className={"product-desc"}>Our best product</p>
+            <Divider height="1" width="100%" bg="#000000" />
+            <div className={"product-price"}>
+              <span>Price:</span>
+              <span>$12</span>
             </div>
-        </Container>
-    </div>)
+            <div className={"button-box"}>
+              <Button variant="contained">Add To Basket</Button>
+            </div>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
